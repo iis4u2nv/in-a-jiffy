0b1d22760ca4f68f51e3e4ef9dd5688e5b9c753c
amazing grace <agracenotary@gmail.com>
2023-09-17 19:29:09 -0400

    corrected the user query inside the revolvers.js file

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -3,7 +3,7 @@ const { User, Book } = require('../models');
 const resolvers = {
   Query: {
     user: async () => {
-      return Tech.find({});
+      return User.find({});
     },
     book: async (parent, { _id }) => {
       const params = _id ? { _id } : {};
